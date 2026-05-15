467c6a12673ee28b75556c377cb7add59b41b405
Christopher Schölzel <christopher.schoelzel@aveyara.com>
2026-05-15 12:08:10 +0200

    fixes linting and typing errors

--- src/apicadabri/recursive.py
+++ src/apicadabri/recursive.py
@@ -1,8 +1,8 @@
 # FUTURE: In Python 3.13 this can be replaced with asyncio.Queue.shutdown
-from abc import ABC, abstractmethod
 import asyncio
+from abc import ABC, abstractmethod
 from collections.abc import AsyncGenerator, Iterable
-from typing import Generic, TypeVar
+from typing import Generic, Self, TypeVar
 
 import aiohttp
 
@@ -12,24 +12,51 @@ A = TypeVar("A")
 R = TypeVar("R")
 
 class PoisonPill:
-    @classmethod
-    def get_instance(cls):
-        if not hasattr(cls, "inst"):
-            cls.inst = PoisonPill()
-        return cls.inst
+    """Poison pill singleton to signal that a queue should be terminated."""
+    _instance = None
+
+    def __new__(cls) -> Self:
+        """Returns the singleton instance of this class."""
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
 
 class ApicadabriRecursiveResponse(ApicadabriBulkResponse[A, R], Generic[A, R], ABC):
-    async def execute_task_group(self):
+    """Response type for recursive calls that can spawn new tasks within the call.
+
+    To use this base class you have to implement the following methods:
+
+    * `call_api` should implement your main task(s). It will receive arguments from
+        the `instances` method and can schedule new tasks via `self.task_group`.
+    * `instances` must return an iterator over the arguments of the main tasks(s).
+
+    Args:
+        A: The type of the arguments that are passed to the API call.
+        R: The return type of the API call.
+    """
+    async def execute_task_group(self) -> None:
+        """Creates a task group to start the initial instances.
+
+        This task group is saved as an instance variable allowing the instances
+        to generate new tasks and schedule them within the same task group.
+
+        This method only returns after all the tasks in the task group have actually finished.‚
+        """
         self.result_queue: asyncio.Queue[R | PoisonPill] = asyncio.Queue()
-        async def worker(client: aiohttp.ClientSession, args: A):
+        async def worker(client: aiohttp.ClientSession, args: A) -> None:
+            """Performs individual calls and puts result into result queue.
+
+            Args:
+                client: A shared client session to use for efficient HTTP calls.
+                args: The arguments to the individual API call.
+            """
             _, res = await self.call_with_semaphore(client, 0, args)
             await self.result_queue.put(res)
-        async with aiohttp.ClientSession() as client:
-            async with asyncio.TaskGroup() as tg:
-                self.task_group = tg
-                for inst in self.instances():
-                    tg.create_task(worker(client, inst))
-        await self.result_queue.put(PoisonPill.get_instance())
+        async with aiohttp.ClientSession() as client, asyncio.TaskGroup() as tg:
+            self.task_group = tg
+            for inst in self.instances():
+                tg.create_task(worker(client, inst))
+        await self.result_queue.put(PoisonPill())
 
     async def call_all(self) -> AsyncGenerator[R, None]:
         """Return an iterator that yields the results of the API calls.
@@ -40,15 +67,14 @@ class ApicadabriRecursiveResponse(ApicadabriBulkResponse[A, R], Generic[A, R], A
         `instances`. However, it also allows to inspect and process results
         as they arrive.
         """
-        next_index = 0
-        buffer: list[tuple[int, R]] = []
         # TODO: Concurrent task receives work items from queue and puts results in second queue, this task receives results from result queue and returns them
-        asyncio.create_task(self.execute_task_group())
+        # result needs to be stored to avoid garbage collection
+        self._main_task = asyncio.create_task(self.execute_task_group())
         result = await self.result_queue.get()
         while not isinstance(result, PoisonPill):
             # TODO maybe we also want to do some re-ordering here in the future?
             yield result
-            result = await self.result_queue.get()            
+            result = await self.result_queue.get()
 
     @abstractmethod
     def instances(self) -> Iterable[A]:
